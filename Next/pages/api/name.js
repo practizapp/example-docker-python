aea4b9eadd0616114c69d257542f512a6386907a
sgnd <sugandaletters@gmail.com>
2022-05-01 23:03:24 +0700

    update

--- Next/pages/api/name.js
+++
@@ -1,6 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-const getRandomName = require('node-random-name');
-
-export default function handler(req, res) {
-  res.status(200).json({ name: getRandomName() });
-}
